463974ebc621d10a2a08168d2ccb0c4cb89f19f6
neilbutler <user1@LRs-MacBook-Pro.local>
2015-01-09 10:59:18 +0000

    add flask app

---
+++ Flask-App/flask-app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello():
+    return 'Hello World!'
+
+@app.route('/squee')
+def squee():
+    return 'Squeelookle!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
